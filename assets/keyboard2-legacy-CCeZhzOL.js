1a5714850ffa2d6b71f223230c9aca4542921d31
Clintharolcodes <elijahmarujahabriol@gmail.com>
2025-02-20 14:19:07 +0800

    Updates

--- assets/keyboard2-legacy-CCeZhzOL.js
+++ assets/keyboard2-legacy-CCeZhzOL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgZk-Db-.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D_nntYk4.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
