093cc7b130d82112fb5c688163edec585d5bb30c
code <elijahmarujahabriol@gmail.com>
2025-02-27 14:25:07 +0800

    Updates

--- assets/swipe-back-legacy-CoVa7DN-.js
+++ assets/swipe-back-legacy-CoVa7DN-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_nntYk4.js","./index3-legacy-BuJbn38c.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-_l8sq0sx.js","./index3-legacy-2UlC95bC.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
