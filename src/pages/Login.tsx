8e38b14053bc32cc64a3614750c9b19c7bab01df
code <elijahmarujahabriol@gmail.com>
2025-02-27 14:22:20 +0800

    added ion-input and ion card

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,94 @@
-import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-  
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
+import {
+  IonAvatar,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonInputPasswordToggle,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+  useIonRouter,
+} from "@ionic/react";
+
+
+import React from 'react';
+import { IonIcon } from '@ionic/react';
+import { logoIonic } from 'ionicons/icons';
+import { IonChip } from '@ionic/react';
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+  const doLogin = () => {
+    navigation.push("/it35-lab/app", "forward", "replace");
+  }
+
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
+    //HEADER
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle></IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "100%",
+                }}
+              >
+                   <IonChip>
+        <IonAvatar>
+          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+        </IonAvatar>
+        <IonLabel>Clintharold</IonLabel>
+      </IonChip>
+              </div>
+              <IonItem>
+                <IonInput
+                  label="Username"
+                  placeholder="enter username"
+                ></IonInput>
+              </IonItem>
+              <IonItem>
+                <IonInput
+                  type="password"
+                  label="Password"
+                  value="sumampong123"
+                >
+                  <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+                </IonInput>
+              </IonItem>
+              <IonButton onClick={() => doLogin()} expand="full">
                 Login
-            </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+              </IonButton>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -8,6 +8,8 @@ import {
       IonToolbar 
   } from '@ionic/react';
   
+  import React from 'react';
+  import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
   const Feed: React.FC = () => {
     return (
       <IonPage>
@@ -20,6 +22,23 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+   
+
+
+
+        <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+
+
+
+
+          
         </IonContent>
       </IonPage>
     );
