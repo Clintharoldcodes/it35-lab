91286da2b758a2423b8e6f483f1f8c0106726286
Clintcodes <sumampongclintharold@gmail.com>
2025-04-24 12:03:52 +0800

    Updates

--- assets/md.transition-legacy-Bm26q0PN.js
+++ assets/md.transition-legacy-Bm26q0PN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B5HjgG1r.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-lli6OvnQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-DY4hKX0y.js
+++ assets/status-tap-legacy-DY4hKX0y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B5HjgG1r.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-lli6OvnQ.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
