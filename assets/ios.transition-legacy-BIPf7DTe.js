1e44f2be19dff68367e1cfc337b860749441a817
Clintcodes <sumampongclintharold@gmail.com>
2025-04-24 12:13:38 +0800

    Updates

--- assets/ios.transition-legacy-BIPf7DTe.js
+++ assets/ios.transition-legacy-BIPf7DTe.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-lli6OvnQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C_S2dD3M.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-tGV3jU1c.js
+++ assets/md.transition-legacy-tGV3jU1c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-lli6OvnQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-C_S2dD3M.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
