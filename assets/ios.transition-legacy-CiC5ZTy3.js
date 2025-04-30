2f00df4e0574fd43a66091f48cee70c25ac6c7ca
Clintharoldcodes <clintharoldsumampong@gmail.com>
2025-04-30 14:36:52 +0800

    Updates

--- assets/ios.transition-legacy-CiC5ZTy3.js
+++ assets/ios.transition-legacy-CiC5ZTy3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C4Gr5hgY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-rYbgd_WS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DyK2Yghd.js
+++ assets/swipe-back-legacy-DyK2Yghd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C4Gr5hgY.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-rYbgd_WS.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
