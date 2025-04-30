99fc510aeb4b2d04d62b050e941daeacb4921959
Clintharoldcodes <clintharoldsumampong@gmail.com>
2025-04-30 13:35:12 +0800

    Updates

--- assets/swipe-back-legacy-73fjgckb.js
+++ assets/swipe-back-legacy-73fjgckb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C_S2dD3M.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BN6IOwXO.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
