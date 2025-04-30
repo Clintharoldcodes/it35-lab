3b6dbb93606d32b7b6823c6d66e7fac497edbf7c
Clintharoldcodes <clintharoldsumampong@gmail.com>
2025-04-30 15:10:12 +0800

    Updates

--- assets/ios.transition-legacy-CWo-qMg7.js
+++ assets/ios.transition-legacy-CWo-qMg7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BNmC9_WO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DfuLKIqi.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
